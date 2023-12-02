b3f1825b8a7e8f441ce6cff2664109d60820d084
vivian3007 <89837057+vivian3007@users.noreply.github.com>
2023-12-02 14:54:01 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+const nn = ml5.neuralNetwork({task: 'regression'})
+nn.load('model/model.json', modelLoaded)
+
+async function modelLoaded() {
+    console.log("the model was loaded!")
+}
+
+let predictButton = document.getElementById('predict')
+predictButton.addEventListener('click', ev => predict(ev))
+
+async function predict(e) {
+    e.preventDefault();
+    let lengthValue = document.getElementById('length').value;
+    let diameterValue = document.getElementById('diameter').value;
+    let heightValue = document.getElementById('height').value;
+    let weightValue = document.getElementById('weight').value;
+    let shuckedWeightValue = document.getElementById('shuckedWeight').value;
+    let visceraWeightValue = document.getElementById('visceraWeight').value;
+    let shellWeightValue = document.getElementById('shellWeight').value;
+
+    const result = await nn.predict({
+        length: parseInt(lengthValue),
+        diameter: parseInt(diameterValue),
+        height:parseInt(heightValue),
+        weight: parseInt(weightValue),
+        shuckedWeight: parseInt(shuckedWeightValue),
+        visceraWeight: parseInt(visceraWeightValue),
+        shellWeight: parseInt(shellWeightValue)
+    })
+
+    console.log(result);
+
+    const predictedAge = result[0].value;
+
+    //calculate years and months
+    const years = Math.floor(predictedAge);
+    const months = Math.round((predictedAge - years) * 12);
+
+    //show the predicted age in years and months
+    if(predictedAge){
+        let prediction = document.getElementById('prediction');
+        prediction.innerHTML = `The predicted age is: ${years} years and ${months} months.`;
+    } else{
+        let prediction = document.getElementById('prediction')
+        prediction.innerHTML = `Fill in the form first!`;
+    }
+
+}
